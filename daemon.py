9a83f20fd5eacdea1865555273569cdd1d587387
neochar <mail@neochar.com>
2019-09-15 16:35:16 +0300

    Daemon added

---
+++ daemon.py
@@ -0,0 +1,23 @@
+
+import subprocess
+import requests
+
+while 1:
+
+    output = subprocess.check_output('mocp -i', shell=True).decode('utf-8')
+
+    lines = str(output).split('\n')
+    for line in lines:
+        if 'Title' in line:
+            title = line.replace('Title:', '')
+            break
+
+    if title:
+        url = 'http://onlinetrackinfo.neochar.com/update.php'
+        r = requests.post(url, data={
+            'songname':title, 
+            'songlength': 313
+            })
+        print(r.text)
+
+    sleep(5)
