2323f2e435ff28c7d8f76850bdaee00f623028d8
neochar <mail@neochar.com>
2019-09-15 16:48:32 +0300

    Fixed song name extracting

--- daemon.py
+++ daemon.py
@@ -1,4 +1,5 @@
 
+import os
 import subprocess
 import requests
 import time
@@ -9,16 +10,19 @@ while 1:
 
     lines = str(output).split('\n')
     for line in lines:
+        if 'File' in line:
+            filename = os.path.basename(line)
         if 'Title' in line:
             title = line.replace('Title:', '')
             break
 
-    if title:
-        url = 'http://onlinetrackinfo.neochar.com/update.php'
-        r = requests.post(url, data={
-            'songname':title, 
-            'songlength': 313
-            })
-        print(r.text)
+    songname = title if title else filename
+
+    url = 'http://onlinetrackinfo.neochar.com/update.php'
+    r = requests.post(url, data={
+        'songname': songname,
+        'songlength': 313
+        })
+    print(r.text)
 
     time.sleep(5)
