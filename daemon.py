b9ff91b9d0ee3ea6b9597491e85a7b58c032519b
neochar <mail@neochar.com>
2019-09-15 16:54:31 +0300

    Updates and fixed

--- daemon.py
+++ daemon.py
@@ -6,23 +6,25 @@ import time
 
 while 1:
 
-    output = subprocess.check_output('mocp -i', shell=True).decode('utf-8')
+    try:
+        output = subprocess.check_output('mocp -i', shell=True).decode('utf-8')
 
-    lines = str(output).split('\n')
-    for line in lines:
-        if 'File' in line:
-            filename = os.path.basename(line)
-        if 'Title' in line:
-            title = line.replace('Title:', '')
-            break
+        lines = str(output).split('\n')
+        for line in lines:
+            if 'File' in line:
+                filename = os.path.basename(line).strip()
+            if 'Title' in line:
+                title = line.replace('Title:', '').strip()
+                break
 
-    songname = title if title else filename
+        songname = title if title else filename
 
-    url = 'http://onlinetrackinfo.neochar.com/update.php'
-    r = requests.post(url, data={
-        'songname': songname,
-        'songlength': 313
-        })
-    print(r.text)
+        url = 'http://onlinetrackinfo.neochar.com/update.php'
+        r = requests.post(url, data={
+            'songname': songname,
+            'songlength': 313
+            })
+    except:
+        pass
 
     time.sleep(5)
